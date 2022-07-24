e153bc7f5145d1903816d7f065d459f8f0ccd241
Seba9292 <sebaszym92@gmail.com>
2022-07-24 13:21:13 +0200

    BankApp

---
+++ src/main/java/AccDetails.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class AccDetails {
+    private String name;
+    private String accNumber;
+    private String login;
+    private long balance;
+    Scanner scanner = new Scanner(System.in);
+
+    public void createAccount(){
+        System.out.println("Provide account name");
+        name = scanner.next();
+        System.out.println("Provide account number");
+        accNumber = scanner.next();
+        System.out.println("Provide login");
+        login = scanner.next();
+        System.out.println("Provide account balance");
+        balance = scanner.nextLong();
+
+
+    }
+    public void showAccount(){
+        System.out.println("Your account name is: " + name);
+        System.out.println("Your account number is: " + accNumber);
+        System.out.println("Your login is: " + login);
+        System.out.println("Your balance is: " + balance);
+
+    }
+
+
+}
---
+++ src/main/java/BankingApp.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class BankingApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AccDetails account = new AccDetails();
+        System.out.println("Witaj w naszej aplikacji bankowej");
+        account.createAccount();
+        int inputNumber;
+        do {
+            System.out.println("Co chcesz zrobic? \n 1.Pokaz informacje o koncie \n 2.Wplata gotowki \n 3.Wyplata gotowki \n 4.Wyjscie");
+            inputNumber = scanner.nextInt();
+            switch (inputNumber) {
+                case 1:
+                    account.showAccount();
+                    break;
+            }
+        }
+                while (inputNumber != 4);
+    }
+
+
+
+}
